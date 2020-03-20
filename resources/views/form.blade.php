a885effba947e304461e0919e5d9f662212320f6
tanyagarg2509 <tanyagarg2509@gmail.com>
2020-03-20 21:47:53 +0530

    AJAX CALL DONE

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -3,7 +3,8 @@
 @section('headerlinks')
 	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	   
+	   <script src="https://code.jquery.com/jquery-3.3.1.min.js">
+		</script>
 	   <meta name="csrf-token" content="{{ csrf_token() }}">
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 @endsection
